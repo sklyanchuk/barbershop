be5c409ed215ca37a358649ef6c5f174f687c947
sklyanchuk <artem.sklyanchuk@gmail.com>
2016-11-26 15:21:01 +0300

    Added style for modal login window

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-var loginForm = document.querySelector('.login-form');
+var loginForm = document.querySelector('.modal-login');
 var loginOpenBtn = document.querySelector('.login');
 function showLoginForm() {
   loginForm.style.display = 'block';
 }
 loginOpenBtn.addEventListener('click', showLoginForm);
 
-var loginCloseBtn = document.querySelector('.login-form-close');
+var loginCloseBtn = document.querySelector('.modal-login-close');
 function closeLoginForm() {
   loginForm.style.display = 'none';
 }
