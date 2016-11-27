387c2f8136e27cb54c85897f7713db36971e450b
sklyanchuk <artem.sklyanchuk@gmail.com>
2016-11-27 12:53:47 +0300

    Added animation for modal window

--- js/script.js
+++ js/script.js
@@ -1,12 +1,42 @@
 var loginForm = document.querySelector('.modal-login');
-var loginOpenBtn = document.querySelector('.login');
-function showLoginForm() {
-  loginForm.style.display = 'block';
-}
-loginOpenBtn.addEventListener('click', showLoginForm);
+var loginFormOpenBtn = document.querySelector('.login');
+var loginFormCloseBtn = document.querySelector('.modal-login-close');
+var inputLogin = loginForm.querySelector('[name=login]');
+var inputPassword = loginForm.querySelector('[name=password]');
+var form = loginForm.querySelector('form');
+var storageLogin = localStorage.getItem('login');
 
-var loginCloseBtn = document.querySelector('.modal-login-close');
-function closeLoginForm() {
-  loginForm.style.display = 'none';
+function saveLogin(event) {
+  if (!(inputLogin.value && inputPassword.value)) {
+    event.preventDefault();
+    loginForm.classList.add('modal-error');
+  } else {
+    localStorage.setItem('login', inputLogin.value);
+  }
 }
-loginCloseBtn.addEventListener('click', closeLoginForm);
+function showLoginForm(event) {
+  event.preventDefault();
+  loginForm.classList.add('modal-login-visible');
+  if (storageLogin) {
+    inputLogin.value = storageLogin;
+    inputPassword.focus();
+  } else {
+    inputLogin.focus();
+  }
+}
+function closeLoginForm(event) {
+  event.preventDefault();
+  loginForm.classList.remove('modal-login-visible');
+}
+function closeLoginFormEsc(event) {
+  if (event.keyCode == 27) {
+    if (loginForm.classList.contains('modal-login-visible')) {
+      loginForm.classList.remove('modal-login-visible');
+    }
+  }
+}
+
+form.addEventListener('submit', saveLogin);
+loginFormOpenBtn.addEventListener('click', showLoginForm);
+loginFormCloseBtn.addEventListener('click', closeLoginForm);
+window.addEventListener('keydown', closeLoginFormEsc);
